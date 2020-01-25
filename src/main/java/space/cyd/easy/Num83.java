1973020ba2ed9e4e4d94576ee0f4125e7d14d556
cyd <chenyanda@gmail.com>
2020-01-25 23:00:46 +0800

    add num83 descrip.

--- src/main/java/space/cyd/easy/Num83.java
+++ src/main/java/space/cyd/easy/Num83.java
@@ -4,6 +4,16 @@ package space.cyd.easy;
  * @author: chenyda
  * @date: 2020/01/25 20:24
  * @description:
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ *
+ * Example 1:
+ *
+ * Input: 1->1->2
+ * Output: 1->2
+ * Example 2:
+ *
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
  */
 public class Num83 {
     public ListNode deleteDuplicates(ListNode head) {
