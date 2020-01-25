79a1b8ea73f6f403922b27034c82325ad18337f1
cyd <chenyanda@gmail.com>
2020-01-25 15:57:00 +0800

    add num70.

---
+++ src/test/java/space/cyd/easy/Num70Test.java
@@ -0,0 +1,17 @@
+package space.cyd.easy;
+
+import org.junit.Test;
+
+/**
+ * @author: chenyda
+ * @date: 2020/01/25 15:55
+ * @description:
+ */
+public class Num70Test {
+    @Test
+    public void test(){
+        System.out.println(new Num70().climbStairs(3));
+        System.out.println(new Num70().climbStairs(4));
+        System.out.println(new Num70().climbStairs(5));
+    }
+}
