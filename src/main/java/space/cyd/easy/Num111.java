3e83007c9a7d1fdbaeba18d40ae761c55a939535
cyd <chenyanda@gmail.com>
2020-02-16 14:58:52 +0800

    add num111

---
+++ src/main/java/space/cyd/easy/Num111.java
@@ -0,0 +1,37 @@
+package space.cyd.easy;
+
+/**
+ * @author: chenyda
+ * @date: 2020/02/16 13:55
+ * @description: Given a binary tree, find its minimum depth.
+ * <p>
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ * <p>
+ * Note: A leaf is a node with no children.
+ * <p>
+ * Example:
+ * <p>
+ * Given binary tree [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its minimum depth = 2.
+ */
+public class Num111 {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        int heightL = minDepth(root.left);
+        int heightR = minDepth(root.right);
+        return Math.min(heightL, heightR) + 1;
+    }
+}
